6e4ab58e8ecb49bcd35ddb70a24ecbe7bbb31132
Helmika Priyanto <helmika.zero@gmail.com>
2021-07-23 20:12:23 +0700

    Update DriverSet.cs

--- Assets/Scripts/DriverSet.cs
+++ Assets/Scripts/DriverSet.cs
@@ -6,8 +6,31 @@ using UnityEngine.UI;
 public class DriverSet : MonoBehaviour
 {
     [System.Serializable]
+    public class WarnaMotor
+    {
+        public string warnaText;
+        public Material warnaMat;
+    }
+
+    public WarnaMotor[] warna;
+
+    
+
+    [System.Serializable]
+    public class ModelMotor
+    {
+        public string modelText;
+        public GameObject modelparent;
+    }
+
+    public ModelMotor[] motor;
+
+
+
+    [System.Serializable]
     public class DriverProperties
     {
+
         public bool isAman;
 
         [Header("SIM C")]
@@ -23,19 +46,19 @@ public class DriverSet : MonoBehaviour
         public string stnk_noplat = "D 4912 GX";
         public string stnk_nama = "HARI MONOLO";
         public string stnk_alamat = "JL. KADAL RT 14/93 KELURAHAN, KECEMATAN, BANDUNG";
-        public string stnk_merk = "HANDO";
+        public int stnk_modelIndex = 0;
         public string stnk_jenis = "SEPEDA MOTOR";
         public int stnk_tahunpembuatan = 2017;
         public string stnk_cc = "00150 CC";
         public string stnk_rangka = "MH1KC82193184";
         public string stnk_mesin = "KC82E123455";
-        public string stnk_warna = "MERAH";
+        public int stnk_warnaIndex = 0;
         public int[] stnk_kadaluarsa = new int[3] { 31, 7, 2021 };
 
         [Header("MOTOR PROPS")]
-        public string motor_noplat = "D 4912 GX";
-        public int motor_matindex = 0;
         public int motor_model = 0;
+        public string motor_noplat = "D 4912 GX";
+        public int motor_warnaIndex = 0;
         public string motor_mesin = "KC82E123455";
         public bool motor_spion = true;
         public bool motor_lampudepan = true;
@@ -68,14 +91,12 @@ public class DriverSet : MonoBehaviour
     public Text wstnk_kadaluarsa;
 
     [Header("WORLD MOTOR PROPS")]
-    public Text wmotor_noplat;
-
-    public GameObject[] wmotor_model;
-
-    public Text wmotor_mesin;
-    public GameObject wmotor_spion;
-    public GameObject wmotor_lampudepan;
-    public GameObject wmotor_lampubelakang;
+    public int wmotor_modelIndex;
+    public Text[] wmotor_noplat;
+    public Text[] wmotor_mesin;
+    public GameObject[] wmotor_spion;
+    public GameObject[] wmotor_lampudepan;
+    public GameObject[] wmotor_lampubelakang;
 
     // Start is called before the first frame update
     void Start()
@@ -99,7 +120,29 @@ public class DriverSet : MonoBehaviour
         wsim_pekerjaan.text = drivers[driverIndex].sim_pekerjaan;
         wsim_kadaluarsa.text = drivers[driverIndex].sim_kadaluarsa[0] + "-" + drivers[driverIndex].sim_kadaluarsa[1] + "-" + drivers[driverIndex].sim_kadaluarsa[2];
 
+        wstnk_noplat.text = drivers[driverIndex].stnk_noplat;
+        wstnk_nama.text= drivers[driverIndex].stnk_nama ;
+        wstnk_alamat.text= drivers[driverIndex].stnk_alamat;
+        wstnk_merk.text= motor[drivers[driverIndex].stnk_modelIndex].modelText;
+        wstnk_jenis.text= drivers[driverIndex].stnk_jenis;
+        wstnk_tahunpembuatan.text= drivers[driverIndex].stnk_tahunpembuatan.ToString();
+        wstnk_cc.text= drivers[driverIndex].stnk_cc;
+        wstnk_rangka.text= drivers[driverIndex].stnk_rangka;
+        wstnk_mesin.text= drivers[driverIndex].stnk_mesin;
+        wstnk_warna.text = warna[drivers[driverIndex].stnk_warnaIndex].warnaText;
+        wstnk_kadaluarsa.text= drivers[driverIndex].stnk_kadaluarsa.ToString();
 
+        foreach(ModelMotor mot in motor)
+        {
+            mot.modelparent.SetActive(false);
+        }
+
+        motor[wmotor_modelIndex].modelparent.SetActive(true);
+        wmotor_noplat[wmotor_modelIndex].text = drivers[driverIndex].motor_noplat;
+        wmotor_mesin[wmotor_modelIndex].text = drivers[driverIndex].motor_mesin;
+        wmotor_spion[wmotor_modelIndex].SetActive(drivers[driverIndex].motor_spion);
+        wmotor_lampudepan[wmotor_modelIndex].SetActive(drivers[driverIndex].motor_lampudepan);
+        wmotor_lampubelakang[wmotor_modelIndex].SetActive(drivers[driverIndex].motor_lampubelakang);
 
 
     }
